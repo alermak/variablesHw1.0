b8eff4cc6da0718dfbac22dbf81bfd4283ba585e
alermak <alermakoriginal@gmail.com>
2022-06-13 17:00:38 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 150;
+        int c = 231547867;
+        long d = 419229;
+        float e = 3.4535f;
+        double f = 3.95768549f;
+
+        float boxerJohnWeight = 78.2f;
+        float boxerJimWeight = 82.7f;
+        float totalWeight = boxerJohnWeight + boxerJimWeight;
+        System.out.println("Total weight of John and Jim: " + totalWeight + " kg.");
+
+        float weightDifference = boxerJimWeight - boxerJohnWeight;
+        System.out.println(" Weight difference between Jonh and Jim: " + weightDifference + " kg.");
+
+
+
+
+
+    }
+
+
+}
